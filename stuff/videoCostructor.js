8bec29332f8a9c1edaeac46f8880d4ab789f1fe9
CesarPaiz <gonzalezcesar028@gmail.com>
2023-09-28 17:47:40 -0600

    No display subtitles if not able to get one

--- stuff/videoCostructor.js
+++ stuff/videoCostructor.js
@@ -6,41 +6,100 @@ class VideoPlayer extends Component {
   render() {
     const { subsES, subsEN, videoURL } = this.props;
 
-    return (
-      <div>
-        {videoURL && (
-          <ReactPlayer
-            url={videoURL}
-            key={videoURL}
-            controls={true}
-            width="100%"
-            height="auto"
-            config={{
-              file: {
-                attributes: {
-                  crossOrigin: 'true',
+    if (!subsES && subsEN) {
+      return (
+        <div>
+          {videoURL && (
+            <ReactPlayer
+              url={videoURL}
+              key={videoURL}
+              controls={true}
+              width="100%"
+              height="auto"
+              config={{
+                file: {
+                  attributes: {
+                    crossOrigin: 'true',
+                  },
+                  tracks: [
+                    {
+                      kind: 'subtitles',
+                      src: subsEN,
+                      srcLang: 'en',
+                      label: 'English',
+                      default: true,
+                    },
+                  ],
                 },
-                tracks: [
-                  {
-                    kind: 'subtitles',
-                    src: subsES,
-                    srcLang: 'es',
-                    label: 'Spanish',
-                    default: true,
+              }}
+            />
+          )}
+        </div>
+      );
+    }
+    else if (!subsEN || !subsES) {
+      return (
+        <div>
+          {videoURL && (
+            <ReactPlayer
+              url={videoURL}
+              key={videoURL}
+              controls={true}
+              width="100%"
+              height="auto"
+              config={{
+                file: {
+                  attributes: {
+                    crossOrigin: 'true',
+                  }
+                }
+              }
+              }
+
+            />
+          )}
+        </div>  
+      )
+
+    }
+    else {
+      return (
+        <div>
+          {videoURL && (
+            <ReactPlayer
+              url={videoURL}
+              key={videoURL}
+              controls={true}
+              width="100%"
+              height="auto"
+              config={{
+                file: {
+                  attributes: {
+                    crossOrigin: 'true',
                   },
-                  {
-                    kind: 'subtitles',
-                    src: subsEN,
-                    srcLang: 'en',
-                    label: 'English',
-                  },
-                ],
-              },
-            }}
-          />
-        )}
-      </div>
-    );
+                  tracks: [
+                    {
+                      kind: 'subtitles',
+                      src: subsES,
+                      srcLang: 'en',
+                      label: 'Spanish',
+                      default: true,
+                    },
+                    {
+                      kind: 'subtitles',
+                      src: subsEN,
+                      srcLang: 'en',
+                      label: 'English',
+                    },
+                  ],
+                },
+              }}
+            />
+          )}
+        </div>
+      );
+    }
+
   }
 }
 
