f7ccb100b9df3383343fb427eb6628d83e99c2df
CesarPaiz <gonzalezcesar028@gmail.com>
2023-09-29 21:16:37 -0600

    Add responsive to vanBar

--- src/app/NavBar.js
+++ src/app/NavBar.js
@@ -10,9 +10,10 @@ export default function NavBar() {
     const [nombreAnilist, setNombre] = useState("")
     return (
         <nav id="main_nav" className="flex gap-4 w-full flex-row mt-3 max-h-10 justify-center">
-            <div className="font-size-2 text-white text-3xl flex-row flex place-items-left ml-4 mt-1 font-bold pl-4">
+            <div className="font-size-2 text-white text-3xl flex-row flex place-items-left  md:ml-4 mt-1 font-bold pl-4">
                 <Link className="" href="/" >
-                    <Image width={180} height={80} className="" src="/img/logoTitle.png" alt="logo" />
+                    <span className="text-white hidden lg:block">AniMaid</span>
+                    <span className="text-white md:hidden ">AM</span>
                 </Link>
             </div>
             <div className="flex flex-row justify-center grow ">
@@ -26,12 +27,12 @@ export default function NavBar() {
                         },
                     }}
                 >
-                    <button className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Buscar</button>
+                    <button className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-1 md:px-4 rounded-full">Buscar</button>
                 </Link>
             </div>
 
             <div>
-                <button className="flex place-items-center outline outline-offset-1 outline-1 outline-blue-500 bg-none hover:bg-none  rounded-full  mx-6">
+                <button className="flex w-10 h-10 place-items-center outline outline-offset-1 outline-1 outline-blue-500 bg-none hover:bg-none  rounded-full  mx-2 md:mx-8">
                     <Image width={50} height={50} className=" w-10 h-10 bg-slate-800  rounded-full p-0" src="/img/animeico.png" alt="logo" />
                 </button>
             </div>
