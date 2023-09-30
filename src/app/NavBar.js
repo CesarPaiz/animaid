20396543a7b791b70f753a315cb75486bc6c30d9
CesarPaiz <gonzalezcesar028@gmail.com>
2023-09-29 21:25:39 -0600

    Add responsive to vanBar

--- src/app/NavBar.js
+++ src/app/NavBar.js
@@ -10,14 +10,14 @@ export default function NavBar() {
     const [nombreAnilist, setNombre] = useState("")
     return (
         <nav id="main_nav" className="flex gap-4 w-full flex-row mt-3 max-h-10 justify-center">
-            <div className="font-size-2 text-white text-3xl flex-row flex place-items-left  md:ml-4 mt-1 font-bold pl-4">
+            <div className="font-size-2 text-white text-3xl flex-row flex place-items-left ml-0 md:ml-4 mt-1 font-bold pl-4">
                 <Link className="" href="/" >
                     <span className="text-white hidden lg:block">AniMaid</span>
                     <span className="text-white md:hidden ">AM</span>
                 </Link>
             </div>
             <div className="flex flex-row justify-center grow ">
-                <input onFocus={e => e.target.placeholder =""}onBlur={e => e.target.placeholder = "¿Que anime debo buscar?"} value={nombreAnilist} onChange={e => setNombre(e.target.value)} className=" flex rounded-full text-dark-500  place-items-center justify-center grow place-items-center  mr-2 text-center outline-none max-w-lg" placeholder="¿Que anime debo buscar?" />
+                <input className=" flex rounded-full text-dark-500  place-items-center justify-center grow place-items-center  mr-2 text-center outline-none max-w-lg" onFocus={e => e.target.placeholder =""}onBlur={e => e.target.placeholder = "¿Que anime debo buscar?"} value={nombreAnilist} onChange={e => setNombre(e.target.value)}  placeholder="¿Que anime debo buscar?" />
 
                 <Link
                     href={{
@@ -32,7 +32,7 @@ export default function NavBar() {
             </div>
 
             <div>
-                <button className="flex w-10 h-10 place-items-center outline outline-offset-1 outline-1 outline-blue-500 bg-none hover:bg-none  rounded-full  mx-2 md:mx-8">
+                <button className="flex w-10 h-10 place-items-center outline outline-offset-1 outline-1 outline-blue-500 bg-none hover:bg-none  rounded-full  mr-3 md:mx-8">
                     <Image width={50} height={50} className=" w-10 h-10 bg-slate-800  rounded-full p-0" src="/img/animeico.png" alt="logo" />
                 </button>
             </div>
