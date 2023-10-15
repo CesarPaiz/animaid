dab4acf32875310fa6fad1e743e110eb64ab1608
CesarPaiz <gonzalezcesar028@gmail.com>
2023-10-14 22:09:23 -0600

    Return to only one source

--- src/app/anime/mirar/VideoIframe.js
+++ src/app/anime/mirar/VideoIframe.js
@@ -1,19 +1,13 @@
-'use client'
 import PaginationMirar from "../../../../stuff/paginationMirar"
-import { useState } from "react"
 export default function VideoIframe({ jsonVideos, parametros }) {
 
-    const [fuente, setFuente] = useState("")
-
-
-
-
+var fuente = jsonVideos[0].url
     return (
         <>
             <div className="flex flex-row justify-center mt-6 ">
                 {jsonVideos.map(item => (
 
-                    <button key={item.url} onClick={() => setFuente(item.url)} className=" justify-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mr-4">{item.name}</button>
+                    <button key={item.url}  className=" justify-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mr-4">{item.name}</button>
 
                 ))}
             </div>
