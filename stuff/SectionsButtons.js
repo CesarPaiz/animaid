6bde29b7b54a20113716a4a4ae2b5bd30b4c57f7
CesarPaiz <gonzalezcesar028@gmail.com>
2023-10-15 18:49:28 -0600

    Buttons multi source

--- stuff/SectionsButtons.js
+++ stuff/SectionsButtons.js
@@ -23,7 +23,6 @@ export default function SectionButtons() {
         )
             }
         else{
-            //      no retornar nada     //
         }
     
 }
--- stuff/pagination.js
+++ stuff/pagination.js
@@ -7,8 +7,26 @@ export default function PaginationMain({
 }) {
     return (
         <div className="flex justify-center mt-4 text-white border-spacing-2 mb-4">
-            <Bback  actual={paginaQ} />
-            <Bnext  actual={paginaQ} />
+            <Link href={{
+                pathname: '/tendencia',
+                query: {
+                    pagina: paginaQ - 1
+                }
+            }} >
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mr-4" >Anterior</button>
+
+            </Link>
+
+            <Link href={{
+                pathname: '/tendencia',
+                query: {
+                    pagina: paginaQ + 1
+                }
+            }} >
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mr-4" >Siguiente</button>
+
+            </Link>
         </div>
+
     )
 }
