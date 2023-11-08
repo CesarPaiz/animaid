54d07f9bc4b1012d72de22bddd356f523bef46d7
CesarPaiz <gonzalezcesar028@gmail.com>
2023-11-07 22:01:33 -0600

    Add margin to loading gif

--- src/app/EndBar.js
+++ src/app/EndBar.js
@@ -1,6 +1,6 @@
 export default function EndBar() {
     return (
-        <div className="flex bg-gray-900 justify-center align-center text-center ">
+        <div className="flex bg-gray-900 justify-center align-center text-center sticky bottom-0 ">
             <span className="text-white "> AniMaid es un sitio web que funciona como un indexador de contenido de otras páginas. No es propietario de ningún elemento que se muestra en su plataforma, simplemente proporciona enlaces y acceso a recursos de otras fuentes. ♥️ </span>
         </div>
     )
--- src/app/anime/[animeID]/[aniCap]/loading.js
+++ src/app/anime/[animeID]/[aniCap]/loading.js
@@ -1,7 +1,7 @@
 import Image from "next/image"
 export default function Loading() {
     return (
-        <div className="flex justify-center align-center place-self-center mt-4 place-items-center">
+        <div className="flex justify-center align-center place-self-center mt-8 mb-8 place-items-center">
             <Image src="/img/loading.gif" alt="loading" width={200} height={200} />
         </div>
     )
