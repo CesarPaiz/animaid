90eab0069a433559f61684e9584352f9f33258de
CesarPaiz <gonzalezcesar028@gmail.com>
2023-11-07 21:39:07 -0600

    Google verification

--- src/app/HeadHTML.js
+++ src/app/HeadHTML.js
@@ -3,6 +3,7 @@ export default function HeadHTML() {
     
     return (
         <head>
+            <meta name="google-site-verification" content="mFGYXf645OXajg0mWZFPyLjaRcr26MHFKQ4bygKXLOg" />
             <link rel="icon" href="/favicon.png" />
         </head>
     )
